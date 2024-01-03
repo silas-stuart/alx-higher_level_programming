8fdc638571a6f2ba6444678c26a6ae3aada48e8f
silas-stuart <silasbenza16@gmail.com>
2024-01-03 01:22:34 -0800

    Characters to uppercase chars

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 8-uppercase.py file
+
+"""Print a string to uppercase letters."""
+
+def uppercase(str):
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            i = chr(ord(i) - 32)
+        print("{}".format(i), end="")
+    print("")
