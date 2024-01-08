f70ae572eb4da35b504bf84fac32b9f8a676a1a1
silas-stuart <silasbenza16@gmail.com>
2024-01-08 01:27:55 -0800

    New list and no c

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    final_result = ""
+    for l in my_string:
+        if l == "c" or l == "C":
+            continue
+        final_result += p
+    return final_result
